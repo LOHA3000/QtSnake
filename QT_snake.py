396f821c4946486d87c2dafe44da22359b970cfa
Lesha <amp.lesha@gmail.com>
2019-12-05 02:01:10 +0300

    окно

--- QT_snake.py
+++ QT_snake.py
@@ -14,6 +14,27 @@ from PIL import Image
 class Game(QWidget):
     def __init__(self):
         super().__init__()
+        self.n = 10  # размер поля в клеточках
+        self.pixels_side_size = 20  # размер клетки в пикселях
+        self.snake_size = 2  # начальная длина змейки
+
+        self.coordinates = {'head': [0, 0], 'tail': [0, 0], 'pineapple': [0, 0]}
+        self.turns = {}
+        self.directions = {'head': 0}
+
+        self.t = []  # последнее положение хвоста
+        self.ps = []  # последне положение сегмента перед хвостом
+        self.pt = []  # коордтната последнего поворота
+
+        self.play = False
+
+        self.initUI()
+
+    def initUI(self):
+        k = self.pixels_side_size * self.n  # размер поля в пикселях
+
+        self.setFixedSize(k, k)
+        self.setWindowTitle('Змейка')
 
 
 if __name__ == '__main__':
